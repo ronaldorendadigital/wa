a15644632be8e0891c1b58f21707123d657f0bd7
Ronaldo Adriano Marques da Silva <112972922+ronaldorendadigital@users.noreply.github.com>
2022-09-21 10:05:42 -0400

    documentação user
    documentação user

--- backend/src/database/seeds/20200904070004-create-default-users.ts
+++ backend/src/database/seeds/20200904070004-create-default-users.ts
@@ -6,8 +6,8 @@ module.exports = {
       "Users",
       [
         {
-          name: "Press-Ticket",
-          email: "admin@pressticket.com.br",
+          name: "whatende",
+          email: "admin@whatende.link",
           passwordHash: "$2a$08$WaEmpmFDD/XkDqorkpQ42eUZozOqRCPkPcTkmHHMyuTGUOkI8dHsq",
           profile: "admin",
           tokenVersion: 0,
