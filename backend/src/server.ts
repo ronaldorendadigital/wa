de2944e8dd6e85472c76d6807a221fc0e97d3941
Ronaldo Adriano Marques da Silva <112972922+ronaldorendadigital@users.noreply.github.com>
2022-09-21 10:24:29 -0400

    server ts
    server ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,15 +3,6 @@ import app from "./app";
 import { initIO } from "./libs/socket";
 import { logger } from "./utils/logger";
 import { StartAllWhatsAppsSessions } from "./services/WbotServices/StartAllWhatsAppsSessions";
-import swaggerUi from "swagger-ui-express";
-
-import swaggerDocs from "./swagger.json";
-
-var options = {
-  customCss: '.swagger-ui .topbar { display: none }'
-};
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs, options));
 
 const server = app.listen(process.env.PORT, () => {
   logger.info(`Servidor iniciado na porta: ${process.env.PORT}`);
